bf7cf1d8749c1a2b97c7c53afe1472db16af2c2c
ViktorD <vdimitrenko96@gmail.com>
2022-12-17 19:19:46 +0700

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,6 @@ public class Main {
         int oneMileRub = 20;
         int totalMiles = ticket / oneMileRub;
         System.out.println(totalMiles + " - Это Ваши бонусные мили. Спасибо за покупку!");
-
-        //Задание 2. «Получи рубль за каждые 100»
-
-        int initialAccountMoney = 350;
-        int topUpMoney = 300;
-        int bonusMoney = topUpMoney / 100;
-        int totalMoney = initialAccountMoney + topUpMoney + bonusMoney;
-        if (topUpMoney > 1000) {
-            System.out.println(totalMoney + " - Сумма Вашего счёта. " + bonusMoney + " - Сумма Ваших Бонусов");
-        } else {
-            System.out.println(initialAccountMoney + topUpMoney + " - Сумма Вашего счёта");
         }
 
     }
-}
